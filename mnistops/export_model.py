af020989904238ebaedccf7f2167d1ab3ca4dec3
Iliia Petryashin <petryashin.i.e@mail.ru>
2023-12-24 17:12:28 +0300

    Feat: pretrained model to onnx export

--- mnistops/export_model.py
+++ mnistops/export_model.py
@@ -2,20 +2,36 @@ import shutil
 from pathlib import Path
 
 import torch
+from dvc.api import DVCFileSystem
 from hydra import compose, initialize
 
 from .model import MNISTModel
 
 
-def run_export_2_onnx(cfg):
-    best_model_name = (
-        Path(cfg.artifacts.checkpoint.dirpath)
-        / cfg.loggers.experiment_name
-        / "best.txt"
-    )
+def load_pretained_models(pretrained_repo):
+    if not Path(pretrained_repo).exists():
+        fs = DVCFileSystem(".", subrepos=True, rev="master")
+        fs.get(pretrained_repo, pretrained_repo, recursive=True)
+        print("Pretrainded models downloaded")
+    else:
+        print("Models folder already exists")
 
-    with open(best_model_name, "r") as f:
-        best_checkpoint_name = f.readline()
+
+def run_export_2_onnx(cfg):
+    if cfg.pretrained.use:
+        load_pretained_models(cfg.pretrained.dirpath)
+        best_checkpoint_name = (
+            Path(cfg.pretrained.dirpath) / cfg.pretrained.model
+        )
+    else:
+        best_model_name = (
+            Path(cfg.artifacts.checkpoint.dirpath)
+            / cfg.loggers.experiment_name
+            / "best.txt"
+        )
+
+        with open(best_model_name, "r") as f:
+            best_checkpoint_name = f.readline()
 
     # Getting a torch-model
     model = MNISTModel.load_from_checkpoint(best_checkpoint_name)
